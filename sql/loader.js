8c8e3c74768a2bdff9c89b69c7a8a0eb1ab03079
isabello <isabella@lightcurve.io>
2017-10-27 14:12:06 +0200

    Fix loader

--- sql/loader.js
+++ sql/loader.js
@@ -5,7 +5,7 @@ var LoaderSql = {
 
 	getGenesisBlock: 'SELECT "block_id", "payload_hash", "signature" FROM blocks WHERE "height" = 1',
 
-	countMemAccounts: 'SELECT COUNT(*)::int FROM accounts a WHERE a."transaction_id"',
+	countMemAccounts: 'SELECT COUNT(*)::int FROM accounts a WHERE a."transaction_id" IS NOT NULL',
 
 	updateMemAccounts: 'UPDATE mem_accounts SET "u_isDelegate" = "isDelegate", "u_secondSignature" = "secondSignature", "u_username" = "username", "u_balance" = "balance", "u_delegates" = "delegates", "u_multisignatures" = "multisignatures", "u_multimin" = "multimin", "u_multilifetime" = "multilifetime" WHERE "u_isDelegate" <> "isDelegate" OR "u_secondSignature" <> "secondSignature" OR "u_username" <> "username" OR "u_balance" <> "balance" OR "u_delegates" <> "delegates" OR "u_multisignatures" <> "multisignatures" OR "u_multimin" <> "multimin" OR "u_multilifetime" <> "multilifetime";',
 
@@ -13,7 +13,7 @@ var LoaderSql = {
 
 	getDelegates: 'SELECT ENCODE("publicKey", \'hex\') FROM delegates',
 
-	validateMemBalances: 'SELECT * FROM validate_mem_balances()',
+	validateMemBalances: 'SELECT * FROM validate_accounts_balances()',
 
 	getRoundsExceptions: 'SELECT * FROM rounds_exceptions;'
 };
