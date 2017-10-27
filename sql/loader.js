355a1205f29856639b8f692aad146914d178ef58
isabello <isabella@lightcurve.io>
2017-10-27 14:02:48 +0200

    Fix loader

--- sql/loader.js
+++ sql/loader.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var LoaderSql = {
-	countBlocks: 'SELECT COUNT("rowId")::int FROM blocks',
+	countBlocks: 'SELECT COUNT("row_id")::int FROM blocks',
 
-	getGenesisBlock: 'SELECT "id", "payloadHash", "blockSignature" FROM blocks WHERE "height" = 1',
+	getGenesisBlock: 'SELECT "block_id", "payload_hash", "signature" FROM blocks WHERE "height" = 1',
 
-	countMemAccounts: 'SELECT COUNT(*)::int FROM mem_accounts WHERE "blockId" = (SELECT "id" FROM "blocks" ORDER BY "height" DESC LIMIT 1)',
+	countMemAccounts: 'SELECT COUNT(*)::int FROM accounts a WHERE a."transaction_id" = (SELECT "id" FROM "blocks" ORDER BY "height" DESC LIMIT 1)',
 
 	updateMemAccounts: 'UPDATE mem_accounts SET "u_isDelegate" = "isDelegate", "u_secondSignature" = "secondSignature", "u_username" = "username", "u_balance" = "balance", "u_delegates" = "delegates", "u_multisignatures" = "multisignatures", "u_multimin" = "multimin", "u_multilifetime" = "multilifetime" WHERE "u_isDelegate" <> "isDelegate" OR "u_secondSignature" <> "secondSignature" OR "u_username" <> "username" OR "u_balance" <> "balance" OR "u_delegates" <> "delegates" OR "u_multisignatures" <> "multisignatures" OR "u_multimin" <> "multimin" OR "u_multilifetime" <> "multilifetime";',
 
@@ -13,7 +13,7 @@ var LoaderSql = {
 
 	getDelegates: 'SELECT ENCODE("publicKey", \'hex\') FROM mem_accounts WHERE "isDelegate" = 1',
 
-	validateMemBalances: 'SELECT * FROM validateMemBalances()',
+	validateMemBalances: 'SELECT * FROM validate_mem_balances()',
 
 	getRoundsExceptions: 'SELECT * FROM rounds_exceptions;'
 };
