fd6e5f95600b3e6dc834db6a0d5ded02d85b426f
isabello <isabella@lightcurve.io>
2017-10-27 13:03:22 +0200

    Relocate trigger for blocks insert

--- sql/migrations/20171022144031_recreate_functions.sql
+++ sql/migrations/20171022144031_recreate_functions.sql
@@ -6,15 +6,6 @@ DROP TRIGGER block_insert_delete ON blocks;
 DROP FUNCTION delegates_forged_blocks_cnt_update();
 DROP FUNCTION delegates_voters_cnt_update();
 
-
--- Create trigger that will execute 'delegates_forged_blocks_cnt_update' after insertion or deletion of block
-CREATE CONSTRAINT TRIGGER block_insert_delete
-	AFTER INSERT OR DELETE ON blocks
-	DEFERRABLE INITIALLY DEFERRED
-	FOR EACH ROW
-	EXECUTE PROCEDURE delegates_forged_blocks_count_update();
-
-
 CREATE OR REPLACE FUNCTION public.delegate_change_ranks_update()
  RETURNS trigger
  LANGUAGE plpgsql
@@ -26,6 +17,13 @@ CREATE OR REPLACE FUNCTION public.delegates_forged_blocks_count_update()
  LANGUAGE plpgsql
 AS $function$ BEGIN IF (TG_OP = 'INSERT') THEN UPDATE delegates SET blocks_forged_count = blocks_forged_count+1 WHERE public_key = NEW."generator_public_key"; ELSIF (TG_OP = 'DELETE') THEN UPDATE delegates SET blocks_forged_count = blocks_forged_count-1 WHERE public_key = OLD."generator_public_key"; END IF; RETURN NULL; END $function$
 ;
+
+-- Create trigger that will execute 'delegates_forged_blocks_cnt_update' after insertion or deletion of block
+CREATE CONSTRAINT TRIGGER block_insert_delete
+	AFTER INSERT OR DELETE ON blocks
+	DEFERRABLE INITIALLY DEFERRED
+	FOR EACH ROW
+	EXECUTE PROCEDURE delegates_forged_blocks_count_update();
 
 CREATE OR REPLACE FUNCTION public.delegates_rank_update()
  RETURNS TABLE(updated integer)
